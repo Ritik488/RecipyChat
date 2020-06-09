251621814b492ce0413e0e77e8e42de9e3431103
Ritik <ritikmiglani488@gmail.com>
2020-06-09 18:48:30 +0530

    changes

--- lib/Screens/ChatScreen.dart
+++ lib/Screens/ChatScreen.dart
@@ -2,16 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+Firestore _firestore = Firestore.instance;
+FirebaseUser loggedInUser;
 class ChatScreen extends StatefulWidget {
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  Firestore _firestore =Firestore.instance;
+  final messageTextController = TextEditingController();
   final _auth = FirebaseAuth.instance;
-  FirebaseUser loggedInUser;
+  
   String messageText;
+  final _formKey = GlobalKey<FormState>();
   @override
   void initState() {
     super.initState();
@@ -49,41 +52,146 @@ class _ChatScreenState extends State<ChatScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
+          MessageStream(),
           Container(
               decoration: BoxDecoration(
                 color: Colors.blue[50],
               ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      onChanged: (value) {
-                        messageText = value;
-                      },
-                      decoration: InputDecoration(
-                        hintText: 'Type a message',
-                        border: OutlineInputBorder(borderSide: BorderSide.none),
-                        enabledBorder:
-                            OutlineInputBorder(borderSide: BorderSide.none),
+              child: Form(
+                key:_formKey,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: messageTextController,
+                        onChanged: (value) {
+                          messageText = value;
+                        },
+                        validator: (value) => value.isEmpty? 'Enter a valid message': null,
+                        decoration: InputDecoration(
+                          hintText: 'Type a message',
+                          border: OutlineInputBorder(borderSide: BorderSide.none),
+                          enabledBorder:
+                              OutlineInputBorder(borderSide: BorderSide.none),
+                        ),
                       ),
                     ),
-                  ),
-                  FlatButton(
-                      onPressed: () {
-                        //messageText+loggedInuser
-                        _firestore.collection('messages').add({
-                          'Text':messageText,
-                          'Sender':loggedInUser.email
-                        });
-                      },
-                      child: Text(
-                        'Send',
-                        style: TextStyle(color: Colors.blue),
-                      ))
-                ],
+                    FlatButton(
+                        onPressed: () {
+                          messageTextController.clear();
+                         if(_formKey.currentState.validate()){
+                            _firestore.collection('messages').add({
+                            'Text': messageText,
+                            'Sender': loggedInUser.email,
+                            // 'Timestamp': Firestore.FieldValue.serverTimeStamp(),
+                          });
+                         }
+                        },
+                        child: Text(
+                          'Send',
+                          style: TextStyle(color: Colors.blue),
+                        ))
+                  ],
+                ),
               ))
         ],
       )),
     );
   }
 }
+
+class MessageStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: _firestore.collection('messages').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.lightBlueAccent,
+              ),
+            );
+          }
+          final messages = snapshot.data.documents.reversed;
+          List<MessageBubble> messageBubbles = [];
+          for (var message in messages) {
+            final messageText = message.data['Text'];
+            final messageSender = message.data['Sender'];
+
+            final currentUser=loggedInUser.email;
+
+            final messageBubble =
+                MessageBubble(
+                  sender: messageSender,
+                  message: messageText,
+                  isMe: currentUser==messageSender,
+                );
+            messageBubbles.add(messageBubble);
+          }
+          return Expanded(
+            child: ListView(
+              reverse: true,
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 10.0, vertical: 20.0),
+                children: messageBubbles),
+          );
+        });
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  final String sender;
+  final String message;
+  final bool isMe;
+  MessageBubble({this.sender, this.message, this.isMe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: isMe?CrossAxisAlignment.end:CrossAxisAlignment.start,
+        children: [
+          Text(
+            sender,
+            style: TextStyle(fontSize: 11.0, color: Colors.black54),
+          ),
+          Material(
+            borderRadius: BorderRadius.only(
+              bottomLeft:Radius.circular(30.0),
+              topLeft: isMe? Radius.circular(30.0): Radius.circular(0.0),
+              bottomRight: Radius.circular(30.0),
+              topRight: isMe? Radius.circular(0.0):Radius.circular(30.0)
+              ),
+            elevation: 5.0,
+            color: isMe?Colors.lightBlueAccent:Colors.grey[400],
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Text('$message',
+                  style: TextStyle(fontSize: 15.0, color: Colors.white)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+//Apne liye baad ke liye
+  // void getMessages() async{
+  //   final messages = await _firestore.collection('messages').getDocuments();
+  //   for(var message in messages.documents){
+  //     print(message.data);
+  //   }
+  // }
+
+  // void messageStream() async {
+  //   await for (var snapshot in _firestore.collection('messages').snapshots()) {
+  //     for (var message in snapshot.documents) {
+  //       print(message.data);
+  //     }
+  //   }
+  // }
