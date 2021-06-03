dbc821837467ced366f961fa96f9af6f9c9c450b
Chirag Goel <61325696+chirag-goel360@users.noreply.github.com>
2021-06-03 12:12:20 +0530

    UI

--- lib/Screens/Signup.dart
+++ lib/Screens/Signup.dart
@@ -10,206 +10,211 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
-  final _auth =FirebaseAuth.instance;
+  final _auth = FirebaseAuth.instance;
   String email;
   String password;
   String confirmPassword;
-  bool showSpinner=false;
+  bool showSpinner = false;
+  final _formKey = GlobalKey<FormState>();
 
-  final _formKey= GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+    var sizeWidth = MediaQuery.of(context).size.width / 2 * 1.55;
     return SafeArea(
-          child: Scaffold(
-        backgroundColor: Colors.blue,
+      child: Scaffold(
+        backgroundColor: Colors.blue.shade200,
         body: Stack(
           children: [
             ModalProgressHUD(
               inAsyncCall: showSpinner,
-                child: Padding(
-                padding: const EdgeInsets.all(15.0),
+              child: Padding(
+                padding: EdgeInsets.all(15.0),
                 child: Center(
                   child: Form(
                     key: _formKey,
-                    child:Container(
+                    child: Container(
                       height: 400,
-                  width: 400,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(200)),
-                    color: Color.fromRGBO(255, 255, 255, 0.5),
-                  ),
-                      child:Column(
+                      width: 400,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(200),
+                        ),
+                        color: Color.fromRGBO(
+                          255,
+                          255,
+                          255,
+                          0.5,
+                        ),
+                      ),
+                      child: Column(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          Text('Signup', style: TextStyle(fontSize: 20.0, color: Colors.black),),
+                          Text(
+                            'Signup',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              color: Colors.teal,
+                              fontWeight: FontWeight.w800,
+                            ),
+                          ),
                           Column(
                             children: [
                               Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                              color: Colors.red
-                            ),
-                            height: 50.0,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Icon(Icons.account_circle),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10.0),
+                                  ),
+                                  color: Colors.indigo,
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left:10.0),
-                                  child: Container(
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                                      color: Colors.white
+                                height: 70.0,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Icon(
+                                        Icons.account_circle,
+                                        color: Colors.pink,
+                                      ),
                                     ),
-                                    width: MediaQuery.of(context).size.width/2 *1.55,
-                                    height: 40.0,
-                                    // color: Colors.white,
-                                    child:TextFormField(
-                                decoration: InputDecoration(
-                                        border: OutlineInputBorder(
-                                          borderSide: BorderSide.none
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                        left: 10.0,
+                                      ),
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.all(
+                                            Radius.circular(10.0),
+                                          ),
+                                          color: Colors.white,
                                         ),
-                                        enabledBorder: OutlineInputBorder(
-                                          borderSide: BorderSide.none
+                                        width: sizeWidth,
+                                        height: 60.0,
+                                        child: TextFormField(
+                                          decoration: InputDecoration(
+                                            border: OutlineInputBorder(
+                                              borderSide: BorderSide.none,
+                                            ),
+                                            enabledBorder: OutlineInputBorder(
+                                              borderSide: BorderSide.none,
+                                            ),
+                                            contentPadding: EdgeInsets.all(5.0),
+                                            hintText: 'Enter email',
+                                          ),
+                                          validator: (val) {
+                                            return val.isEmpty
+                                                ? 'Enter a valid Email'
+                                                : null;
+                                          },
+                                          onChanged: (value) {
+                                            email = value;
+                                          },
                                         ),
-                                        contentPadding: EdgeInsets.all(5.0),
-                                hintText: 'Enter email'
-                              ),
-                              keyboardType: TextInputType.emailAddress,
-                              validator: (val) =>
-                                      val.isEmpty 
-                                          ? 'Enter a valid email'
-                                          : null,
-                              onChanged: (value){
-                                email=value;
-                              },
-                            ), ),
-                                )
-                            ],)
-                          ),
-                          SizedBox(height:15.0),
-                         Container(
-                            height: 50.0,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                              color: Colors.red
-                            ),
-                            child: Row(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Icon(Icons.enhanced_encryption),
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left:10.0),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                                      color: Colors.white
-                                    ),
-                                    width: MediaQuery.of(context).size.width/2 *1.55,
-                                    height: 40.0,
-                                    child:TextFormField(
-                              decoration: InputDecoration(
-                                        border: OutlineInputBorder(
-                                          borderSide: BorderSide.none
-                                        ),
-                                        enabledBorder: OutlineInputBorder(
-                                          borderSide: BorderSide.none
-                                        ),
-                                        contentPadding: EdgeInsets.all(5.0),
-                                 hintText: 'Enter Password'
                               ),
-                              validator: (val) =>
-                                      val.isEmpty || val.length < 6
-                                          ? 'Enter a valid password'
-                                          : null,
-                              obscureText: true,
-                              onChanged: (value){
-                                password=value;
-                              },
-                            ), ),
-                                )
-                            ],)
-                          ),
-                          SizedBox(height: 15.0,),
-                          Container(
-                            height: 50.0,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                              color: Colors.red
-                            ),
-                            child: Row(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Icon(Icons.enhanced_encryption),
+                              SizedBox(
+                                height: 20.0,
+                              ),
+                              Container(
+                                height: 70.0,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10.0),
+                                  ),
+                                  color: Colors.indigo,
                                 ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left:10.0),
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                                      color: Colors.white
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Icon(
+                                        Icons.lock,
+                                        color: Colors.pink,
+                                      ),
                                     ),
-                                    width: MediaQuery.of(context).size.width/2 *1.55,
-                                    height: 40.0,
-                                    child:TextFormField(
-                                      decoration: InputDecoration(
-                                        border: OutlineInputBorder(
-                                          borderSide: BorderSide.none
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                        left: 10.0,
+                                      ),
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.all(
+                                            Radius.circular(10.0),
+                                          ),
+                                          color: Colors.white,
                                         ),
-                                        enabledBorder: OutlineInputBorder(
-                                          borderSide: BorderSide.none
+                                        width: sizeWidth,
+                                        height: 60.0,
+                                        child: TextFormField(
+                                          decoration: InputDecoration(
+                                            border: OutlineInputBorder(
+                                              borderSide: BorderSide.none,
+                                            ),
+                                            enabledBorder: OutlineInputBorder(
+                                              borderSide: BorderSide.none,
+                                            ),
+                                            contentPadding: EdgeInsets.all(5.0),
+                                            hintText: 'Enter Password',
+                                          ),
+                                          validator: (val) {
+                                            return val.isEmpty || val.length < 6
+                                                ? 'Enter a valid password'
+                                                : null;
+                                          },
+                                          obscureText: true,
+                                          onChanged: (value) {
+                                            password = value;
+                                          },
                                         ),
-                                        contentPadding: EdgeInsets.all(5.0),
-                                 hintText: 'Confirm Password'
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
-                              validator: (val) =>
-                                      val.isEmpty || val.length < 6
-                                          ? 'Enter a valid password'
-                                          : null,
-                              obscureText: true,
-                              onChanged: (value){
-                                confirmPassword=value;
-                              },
-                            ), ),
-                                )
-                            ],)
-                          ),
-
                             ],
                           ),
                           MaterialButton(
                             height: 60,
-                            shape:CircleBorder(side: BorderSide.none),
+                            shape: CircleBorder(
+                              side: BorderSide.none,
+                            ),
                             color: Colors.green[300],
-                            child:  Icon(CupertinoIcons.forward,size: 30.0,),
-                              onPressed: () async{
-                                setState(() {
-                                  showSpinner=true;
-                                });
-                               try{
-                                 if(_formKey.currentState.validate()){
-                                   final newUser =await _auth.createUserWithEmailAndPassword(email: email, password: password);
-                                    if(newUser!=null){
-                                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => ChatScreen()));
-                                    }
-                                    setState(() {
-                                      showSpinner=false;
-                                    });
-                                 }
-                               }catch(e){
-                                 print(e);
-                               }
+                            child: Icon(
+                              CupertinoIcons.forward,
+                              size: 30.0,
+                            ),
+                            onPressed: () async {
+                              try {
+                                if (_formKey.currentState.validate()) {
+                                  final newUser = await _auth
+                                      .createUserWithEmailAndPassword(
+                                    email: email,
+                                    password: password,
+                                  );
+                                  if (newUser != null) {
+                                    Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                        builder: (context) => ChatScreen(),
+                                      ),
+                                    );
+                                  }
+                                  setState(() {
+                                    showSpinner = false;
+                                  });
+                                }
+                              } catch (e) {
+                                print(e);
                               }
-                              ),
-                      ],)
-                    ) 
+                            },
+                          ),
+                        ],
+                      ),
                     ),
+                  ),
                 ),
               ),
             ),
@@ -219,4 +224,3 @@ class _SignupScreenState extends State<SignupScreen> {
     );
   }
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,25 @@ import 'package:recipychat/Screens/Signup.dart';
 import 'package:recipychat/Screens/WelcomeScreen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Recipy Chatter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      // home:LoginScreen(),
       initialRoute: 'Welcome',
       routes: {
-        'Welcome':(context)=>Welcome(),
-        'Login_Screen': (context)=>LoginScreen(),
-        'Signup_Screen': (context)=>SignupScreen(),
+        'Welcome': (context) => Welcome(),
+        'Login_Screen': (context) => LoginScreen(),
+        'Signup_Screen': (context) => SignupScreen(),
       },
       debugShowCheckedModeBanner: false,
     );
